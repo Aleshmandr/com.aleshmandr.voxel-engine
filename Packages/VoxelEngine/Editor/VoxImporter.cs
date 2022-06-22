8648ffce93d458b6a518115f582838bc1bef9788
Alexander Papchikhin <aleshmandr@gmail.com>
2022-06-22 23:59:57 +0200

    Vox importer WIP

---
+++ Packages/VoxelEngine/Editor/VoxImporter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace VoxelEngine.Editor
+{
+    public class VoxImporter : EditorWindow
+    {
+        [MenuItem("Tools/VoxelEngine/Magica Voxel Importer (.vox)", false)]
+        public static void ShowWindow() {
+            EditorWindow.GetWindow(typeof(VoxImporter));
+        }
+
+        private void OnGUI() {
+            titleContent.text = "Magica Voxel Importer";
+            EditorGUILayout.BeginVertical("Box");
+            EditorGUILayout.LabelField("Load custom palette (optional)");
+            if(GUILayout.Button("Load")) {
+                string filePath = EditorUtility.OpenFilePanel("Load palette", "", "png");
+                if(string.IsNullOrEmpty(filePath)) {
+                    return;
+                }
+                //TODO: Load file
+            }
+            
+            EditorGUILayout.LabelField("Import .vox file");
+            if(GUILayout.Button("Import")) {
+                string filePath = EditorUtility.OpenFilePanel("Import file", "", "vox");
+                if(string.IsNullOrEmpty(filePath)) {
+                    return;
+                }
+                //TODO: Load file
+            }
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
