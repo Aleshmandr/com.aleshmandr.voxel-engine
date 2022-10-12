1ba0819bab3ba0272442231fbc91e6a103e91a82
Alexander Papchikhin <aleshmandr@gmail.com>
2022-10-12 18:12:20 +0200

    Fix

--- Packages/VoxelEngine/Runtime/Destructions/VoxelsContainerJoint.cs
+++ Packages/VoxelEngine/Runtime/Destructions/VoxelsContainerJoint.cs
@@ -3,10 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 namespace VoxelEngine.Destructions
 {
     [RequireComponent(typeof(VoxelsClustersDestructionContainer))] [ExecuteAlways]
@@ -148,14 +144,6 @@ namespace VoxelEngine.Destructions
                 Gizmos.color = GizmoColor;
                 Gizmos.DrawSphere(joints[i].Center, joints[i].Radius);
             }
-        }
-
-        [ContextMenu("FixLegacyJount")]
-        public void FixLegacyJount() {
-            joints = new[] {
-                new JointData(radius, center)
-            };
-            EditorUtility.SetDirty(this.gameObject);
         }
   #endif
     }
