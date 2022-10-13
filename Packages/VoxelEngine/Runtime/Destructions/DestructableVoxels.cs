e504ea283df5f7820580f4ced5d4f0de77a935db
Alexander Papchikhin <aleshmandr@gmail.com>
2022-10-13 12:30:30 +0200

    Editor fix

--- Packages/VoxelEngine/Runtime/Destructions/DestructableVoxels.cs
+++ Packages/VoxelEngine/Runtime/Destructions/DestructableVoxels.cs
@@ -170,7 +170,11 @@ namespace VoxelEngine.Destructions
             switch(destructionCollider) {
                 case DestructionColliderType.Box:
                     if(meshCollider != null) {
+#if UNITY_EDITOR
+                        DestroyImmediate(meshCollider);
+#else
                         Destroy(meshCollider);
+#endif
                     }
                     var boxCollider = gameObject.AddComponent<BoxCollider>();
                     var mesh = voxelsContainer.MeshFilter.sharedMesh;
